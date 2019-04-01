5c766bac8212f0eb966e3a001dff9770a5676025
Kevin Krato <kratok08@gmail.com>
2019-04-01 17:49:44 -0600

    Build starter files

---
+++ src/helpers.js
@@ -0,0 +1,6 @@
+// name export functions here that don't || shouldn't belong in a component. 
+
+// example:
+// export function rando(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
