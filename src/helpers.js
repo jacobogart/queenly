e80958bc482f71d974b6b741f08e6f42b05435a6
tkbacher <tiffanybacher@gmail.com>
2019-04-02 15:14:16 -0600

    Add search() to filter through all bars, shows, and queens

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,14 @@
-// name export functions here that don't || shouldn't belong in a component. 
 
-// example:
-// export function rando(arr) {
-//   return arr[Math.floor(Math.random() * arr.length)];
-// }
+function search(searchQuery) {
+  const results = [];
+  let query = searchQuery.toLowerCase();
+
+  let matchingBars = bars.filter(bar => bar.name.toLowerCase().includes(query));
+  let shows = bars.flatMap(bar => bar.shows);
+  let matchingShows = shows.filter(show.title.toLowerCase().includes(query));
+  let matchingQueens = queens.filter(queen.name.toLowerCase().includes(query));
+
+  matchingBars.forEach(match => results.push(match));
+  matchingShows.forEach(match => results.push(match));
+  matchingQueens.forEach(match = results.push(match));
+}
