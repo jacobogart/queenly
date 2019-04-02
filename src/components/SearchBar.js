3f367167ce3ccc0c0a48be50ea4e3b371f9cf792
tkbacher <tiffanybacher@gmail.com>
2019-04-02 15:53:05 -0600

    Correctly import and export to get search function working Co-authored-by: Jacob Bogart <jacobogart@gmail.com>

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import search from '../helpers.js';
 import "../css/SearchBar.css";
-class SearchBar extends React.Component {
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: ''
+    }
+  }
+
+  handleChange = (e) => {
+    search(e.target.value);
+    this.setState({
+      searchQuery: e.target.value
+    });
+  }
+
   render() {
     return (
       <section className="searchBarContainer">
         <form className="SearchBar">
-          <input type="search" placeholder="Search..." className="searchTerm" />
-          <button type="submit" className="searchButton">
+          <input 
+            onChange={this.handleChange}
+            type="search" 
+            placeholder="Search..." 
+            className="searchTerm" 
+          />
+          <button 
+            type="submit" 
+            className="searchButton">
             <i className="fas fa-search searchIcon" />
           </button>
         </form>
--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,22 @@
+// import bars from '../jacobogart.js';
+// import queens from '../jacobogart.js';
+import data from './jacobogart.js';
 
-function search(searchQuery) {
+const {queens, bars} = data;
+
+export default function search(searchQuery) {
   const results = [];
   let query = searchQuery.toLowerCase();
 
   let matchingBars = bars.filter(bar => bar.name.toLowerCase().includes(query));
   let shows = bars.flatMap(bar => bar.shows);
-  let matchingShows = shows.filter(show.title.toLowerCase().includes(query));
-  let matchingQueens = queens.filter(queen.name.toLowerCase().includes(query));
+  let matchingShows = shows.filter(show => show.title.toLowerCase().includes(query));
+  let matchingQueens = queens.filter(queen => queen.name.toLowerCase().includes(query));
 
   matchingBars.forEach(match => results.push(match));
   matchingShows.forEach(match => results.push(match));
-  matchingQueens.forEach(match = results.push(match));
+  matchingQueens.forEach(match => results.push(match));
+
+  console.log(results)
 }
+
