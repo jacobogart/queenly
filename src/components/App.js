6f97e702894bea5f52a6bc2eefcd5a5f0a6c26cf
Kevin Krato <kratok08@gmail.com>
2019-04-03 13:13:41 -0600

    Change article className

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,40 @@
 import React from "react";
 import SplashPage from "./SplashPage";
-import search from '../helpers.js'
+import search from "../helpers.js";
 // * CSS imports
 import "../css/App.css";
+import NavBar from "./NavBar";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       searchResults: []
-    }
+    };
   }
 
-  updateResults = (query) => {
-    this.setState({
-      searchResults: search(query)
-    }, console.log(this.state.searchResults))
-  }
+  updateResults = query => {
+    this.setState(
+      {
+        searchResults: search(query)
+      },
+      console.log(this.state.searchResults)
+    );
+  };
 
   render() {
     return (
-      <section className="App">
-        <article className="header">
-          <SplashPage
-            updateResults={this.updateResults} />
-        </article>
-      </section>
+      <div>
+        <header className="navBar">
+          <NavBar />
+        </header>
+        <section className="App">
+          <article className="mainContent">
+            <SplashPage updateResults={this.updateResults} />
+          </article>
+        </section>
+        <div className="appBackground" />
+      </div>
     );
   }
 }
