e9edc5939a3437bbf0502f0fb9b651c25ac28dd4
SiimonStark <justinpyktel@gmail.com>
2019-04-06 13:41:19 -0600

    WIP: military time conversion

--- src/components/Show_Main.js
+++ src/components/Show_Main.js
@@ -9,11 +9,72 @@ export default class Show_Main extends Component {
     }
   }
 
+  stringifyReoccuring = (reoccuring) => {
+    return reoccuring ? "Every" : "Only this";
+  }
+
+  convertWhen = (dayOfWeek, frequency, reoccuring) => {
+    let howOften = this.stringifyReoccuring(reoccuring);
+    
+    return `${howOften} ${dayOfWeek.dayOfWeek} on the ${frequency.frequency} of the month.`
+  }
+
+  convertTime = (startTime) => {
+    // let standardTime = startTime.startTime.toString().split('');
+    // if (startTime.startTime >= 1300) {
+    //   console.log('stand', standardTime)
+    //   let hours = parseInt(standardTime.splice(0, 2).join(''));
+    //   standardTime = standardTime.splice(0, 2, (hours - 12).split(''))
+    //   console.log('hours', hours)
+    //   console.log(standardTime)
+    // }
+  }
+
+  // convertTime = (startTime) => {
+  //   console.log('time: ', startTime.startTime.toString().split('').splice(0,0, ':'));
+  //   let time = startTime.startTime.split('').splice(1,0, ':'); // your input
+  //   console.log(time);
+  //   time = time.split(':'); // convert to array
+
+  //   // fetch
+  //   var hours = Number(time[0]);
+  //   var minutes = Number(time[1]);
+
+  //   // calculate
+  //   var timeValue;
+
+  //   if (hours > 0 && hours <= 12) {
+  //     timeValue = "" + hours;
+  //   } else if (hours > 12) {
+  //     timeValue = "" + (hours - 12);
+  //   } else if (hours == 0) {
+  //     timeValue = "12";
+  //   }
+
+  //   timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+  //   timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+  // }
+
   render() {
-    let { name } = this.props.cardData
+    let { name, category, notes, host, dayOfWeek, frequency, startTime, reoccuring }
+      = this.props.cardData;
+
     return (
-      <main className="Show-Main Main_Info">
-        <p>Show {name}</p>
+      <main className="Bar-Main Main_Info">
+        <article className="Content">
+          <h2>{name}</h2>
+          <h4>Type of Show: {category}</h4>
+          <h4>Notes:</h4>
+          <p className="notes">{notes}</p>
+          <p className="hosts">Host(s): {host.join(" & ")}</p>
+          <p>When: </p>
+          <p className="showtime">
+            {this.convertWhen({ dayOfWeek }, { frequency }, { reoccuring })}{" "}
+          </p>
+          <p className="time">
+            At: {this.convertTime({startTime})}
+          </p>
+        </article>
       </main>
     );
   }
