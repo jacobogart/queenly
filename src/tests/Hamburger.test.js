10b59b6732ebcf0561172e0d2fe0c3b2e5663267
Kevin Krato <kratok08@gmail.com>
2019-04-09 15:03:53 -0600

    Pseudo code

---
+++ src/tests/Hamburger.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hamburger from "../components/Hamburger";
+import { shallow } from "enzyme";
+
+let wrapper;
+//Hamburger should exist
+//showMenu should be true on first click
+//showMenu should be false on second click
+
+//input "show all venues" should display all venues on click
+//input "show all shows" should display all shows on click
+//input "show all queens" should display all queens on click
+//input "show all favorites" should display all favorites on click
+
+describe("NavBar", () => {});
