57541a3519b9bf9d1c233ebbc2f6569539f44410
Kevin Krato <kratok08@gmail.com>
2019-04-10 05:54:54 -0600

    NavBar first test

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import Navbar from "../components/NavBar";
+import NavBar from "../components/NavBar";
 import { shallow } from "enzyme";
 
 let wrapper;
 //navbar should exist
+
+describe("<NavBar />"),
+  () => {
+    it("should exist", () => {
+      const wrapper = shallow(<NavBar />);
+      expect(wrapper.exists()).toBe(true);
+    });
+  };
+
 //navbar should have hamburger and favorites contained
 //if not on splashPage, navbar should also have searchBar component
 describe("NavBar", () => {});
