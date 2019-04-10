6239cda835b8b7e84f43eb10dadba6409b106b96
tkbacher <tiffanybacher@gmail.com>
2019-04-10 07:55:16 -0600

    Pass gallery tests

--- src/tests/Gallery.test.js
+++ src/tests/Gallery.test.js
@@ -3,21 +3,41 @@ import ReactDOM from 'react-dom';
 import Gallery from '../components/Gallery.js';
 import { shallow } from 'enzyme';
 
-const [nameOfFunctionHere] = jest.fn();
+const selectSearchResult = jest.fn();
+const bars=[];
+const queens=[];
+const cardType = "Show";
+const cardData = {
+  "category": "drag show",
+  "dayOfWeek": "Sudnay",
+  "frequency": "weekly",
+  "host": ["Chamilla Foxx"],
+  "id": 2101,
+  "imageURL": "https://pbs.twimg.com/media/D1Any4VWkAA86HL.jpg",
+  "reoccuring": true,
+  "startTime": [1400],
+  "name": "Sunday Social"
+}
 
 describe('Gallery', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Gallery />
+      <Gallery 
+      cardData={cardData}
+      cardType={cardType}
+      selectSearchResult={selectSearchResult}
+      bars={bars}
+      queens={queens}
+      />
     );
   });
+
   it('should match snapshop', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
   it('should have default state', () => {
-    expect(wrapper.state()).toEqual({
-      // Put state here
-    });
+    expect(wrapper.state()).toEqual({});
   });
 })
