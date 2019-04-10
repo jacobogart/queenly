36bcb65585cba41fb7ae1c54d80d6ee3f969623b
Kevin Krato <kratok08@gmail.com>
2019-04-10 07:43:34 -0600

    Test onClick diplay menu

--- src/tests/Hamburger.test.js
+++ src/tests/Hamburger.test.js
@@ -2,7 +2,19 @@ import React from "react";
 import Hamburger from "../components/Hamburger";
 import { shallow } from "enzyme";
 
-let wrapper;
+describe("<Hamburger />", () => {
+  it("should exist", () => {
+    const wrapper = shallow(<Hamburger />);
+    expect(wrapper.exists()).toBe(true);
+  });
+  it("on button click should display menu", () => {
+    const wrapper = shallow(<Hamburger />);
+    const button = wrapper.find("i");
+    expect(wrapper.find(".showMenu").exists()).toBe(false);
+    button.simulate("click");
+    expect(wrapper.find(".showMenu").exists()).toBe(true);
+  });
+});
 //Hamburger should exist
 //showMenu should be true on first click
 //showMenu should be false on second click
@@ -11,5 +23,3 @@ let wrapper;
 //input "show all shows" should display all shows on click
 //input "show all queens" should display all queens on click
 //input "show all favorites" should display all favorites on click
-
-describe("NavBar", () => {});
