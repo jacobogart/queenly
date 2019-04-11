a352054c1e6c5613f04a97e04ab487c6db6bc364
jacobogart <jacobogart@gmail.com>
2019-04-10 19:52:30 -0600

    Add five passing Hambuger tests

--- src/tests/Hamburger.test.js
+++ src/tests/Hamburger.test.js
@@ -2,41 +2,47 @@ import React from "react";
 import Hamburger from "../components/Hamburger";
 import { shallow } from "enzyme";
 
-// const mockDisplayAllShows = {
-//   <section className="searchResults">
-//   <button
-//     className="toggle-close"
-//     onClick={this.props.displaySplashPage}
-//   >
-//     <i className="fas fa-times-circle" />
-//   </button>
-//   {results}
-// </section>
-// }
+const mockDisplayAllOfType = jest.fn();
+const mockDisplayAllSearchResults = jest.fn();
 
-describe("<Hamburger />", () => {
-  it("should exist", () => {
-    const wrapper = shallow(<Hamburger />);
-    expect(wrapper.exists()).toBe(true);
+describe("Hamburger", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Hamburger
+        displayAllOfType={mockDisplayAllOfType}
+        displayAllSearchResults={mockDisplayAllSearchResults}
+      />
+    );
   });
-  it("on button click should toggle menu", () => {
-    const wrapper = shallow(<Hamburger />);
-    const button = wrapper.find("i");
-    expect(wrapper.find(".showMenu").exists()).toBe(false);
-    button.simulate("click");
-    expect(wrapper.find(".showMenu").exists()).toBe(true);
-    button.simulate("click");
-    expect(wrapper.find(".showMenu").exists()).toBe(false);
+
+  it("should match snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should have default state", () => {
+    expect(wrapper.state()).toEqual({ showMenu: false });
+  });
+
+  it("should toggle showMenu when toggleBurger is invoked", () => {
+    expect(wrapper.state()).toEqual({ showMenu: false });
+    wrapper.instance().toggleBurger();
+    expect(wrapper.state()).toEqual({ showMenu: true });
+    wrapper.instance().toggleBurger();
+    expect(wrapper.state()).toEqual({ showMenu: false });
+  });
+
+  it("should set showMenu to false when handleMouseOut is invoked", () => {
+    expect(wrapper.state()).toEqual({ showMenu: false });
+    wrapper.instance().toggleBurger();
+    expect(wrapper.state()).toEqual({ showMenu: true });
+    wrapper.instance().handleMouseOut();
+    expect(wrapper.state()).toEqual({ showMenu: false });
+  });
+
+  it("should invoke displayAllOfType and displayAllSearchResults when clickHamburger is invoked", () => {
+    const mockEvent = {target: {value: "Show All Queens"}};
+    wrapper.instance().clickHamburger(mockEvent);
+    expect(mockDisplayAllOfType).toHaveBeenCalledWith("Queens");
+    expect(mockDisplayAllSearchResults).toHaveBeenCalled();
   });
 });
-//Hamburger should exist
-//showMenu should be true on first click
-//showMenu should be false on second click
-
-//---
-//mock data needed
-//---
-//input "show all venues" should display all venues on click
-//input "show all shows" should display all shows on click
-//input "show all queens" should display all queens on click
-//input "show all favorites" should display all favorites on click
